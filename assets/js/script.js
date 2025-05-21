446eca3c7073dbce39c9ce9072b4d6821f51b38c
QGdtsc <qgdtsc@gmail.com>
2025-05-21 14:54:01 +0200

    maj projet

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   let images = document.querySelectorAll(".carousel-item img");
+
+//   images.forEach(img => {
+//     img.style.height = "50vh";
+//     img.style.objectFit = "cover";
+//   });
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let images = document.querySelectorAll(".carousel-item img");
+
+//   images.forEach(img => {
+//     img.style.height = "50vh";
+//     img.style.objectFit = "cover";
+//   });
+// });
