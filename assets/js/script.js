98c51cd67e2b543043080bb8f2f1b7eceb2a8ee1
QGdtsc <qgdtsc@gmail.com>
2025-05-21 17:17:13 +0200

    maj projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,25 @@
 //     img.style.objectFit = "cover";
 //   });
 // });
+
+
+
+// document.getElementById("modal-link").addEventListener("click", function (event) {
+//     event.preventDefault(); // Empêche le lien de naviguer immédiatement
+//     var myModal = new bootstrap.Modal(document.getElementById("exampleModalone"));
+//     myModal.show();
+    
+//     setTimeout(() => {
+//         window.location.href = "index.html";
+//     }, 3000); // Redirection après 3 secondes
+// });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var myModal = document.getElementById("Modal_send_message");
+
+    myModal.addEventListener("hidden.bs.modal", function () {
+        window.location.href = "index.html"; // Redirection après fermeture
+    });
+});
